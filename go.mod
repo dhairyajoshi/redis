c108a9b18bd3dd78855428b37c48c03abea0d703
Dhairya <joshidhairaya2002@gmail.com>
2024-09-01 02:13:34 +0530

    feat: added ping command

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dhairya.com
+
+go 1.23.0
--- main.go
+++ main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt";
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
 func main(){
-	fmt.Println("hello world");
+	fmt.Println("Listening on port :6379")
+
+	l, err := net.Listen("tcp", ":6379");
+	
+	if err!=nil{
+		msg:= fmt.Sprintf("Error occured: %s", err)
+		fmt.Println(msg)
+	}
+
+	conn, err:= l.Accept();
+
+	if err!=nil{
+		msg:= fmt.Sprintf("Error occured: %s", err)
+		fmt.Println(msg)
+	}
+
+	defer conn.Close()
+
+	for {
+		resp := NewResp(conn)
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+	
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+		fmt.Println(value)
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		writer := NewWriter(conn)
+		fmt.Println(command)
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+	
+		result := handler(args)
+
+		writer.Write(result)
+	}
+
 }
